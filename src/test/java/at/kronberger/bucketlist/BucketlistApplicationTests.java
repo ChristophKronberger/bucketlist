5cab3fc47cd05d24a5f1b0c2f770cdc0bd671152
christoph kronberger <c.kronberger@live.at>
2023-03-03 20:52:25 +0100

    inital commit

---
+++ src/test/java/at/kronberger/bucketlist/BucketlistApplicationTests.java
@@ -0,0 +1,13 @@
+package at.kronberger.bucketlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BucketlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
